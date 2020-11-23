ce0d37a33d12c6f2fb294bdc9dc9ab6348eadc1a
pum-pum <54636165+pum-pum@users.noreply.github.com>
2020-11-23 11:34:43 +0100

    Update config.h

--- config.h
+++ config.h
@@ -10,7 +10,7 @@
     { "Host: ",       get_host,                true  }, \
     { "Kernel: ",     get_kernel,              true  }, \
     { "Uptime: ",     get_uptime,              false }, \
-    { "Battery: ",    get_battery_percentage,  false }, \
+   /*{ "Battery: ",    get_battery_percentage,  false },*/ \
     SPACER \
     { "Packages: ",   get_packages_pacman,   false }, \
     { "Shell: ",      get_shell,             false }, \
